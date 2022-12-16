5a70055648e161ed8e9cbdc06a02d49765e33277
abbsamoze <69853489+abbsamoze@users.noreply.github.com>
2022-12-16 14:40:51 +0100

    added power task

---
+++ Samuel/Assets/Scripts/Tasks/CalibrateTask.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalibrateTask : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    Vector3 m_EulerAngleVelocity;
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+        m_EulerAngleVelocity = new Vector3(0, 0, 100);
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+        Debug.Log(m_Rigidbody.rotation);
+    }
+}
